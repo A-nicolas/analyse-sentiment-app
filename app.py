bc57e49b6cad09b1c80fdeb798cd14a8c6e6edae
A. Nicolas <antoine.nicolas.gp@gmail.com>
2024-05-05 17:42:06 +0200

    Update globale du code

--- app.py
+++ app.py
@@ -1,47 +1,25 @@
-# import json
-
-# with open("./twitter_token.json") as file:
-#     data = json.load(file)
-
-# print(data["bearer_token"])
-
+import json
+import tweepy
 from fonctions import *
 
-tweets = """ Il ressemble au footballeur VINICIUS » Ptdrrrrrrrrr 
-SPORT 🔵 Real Madrid : « Vinicius est à un niveau très élevé », prévient Rodrygo ! /
+# Chargement du token pour l'API Twitter
+with open("twitter_token.json") as file:
+    data = json.load(file)
 
-👉 Lire l'article :  https://t.co/FbBrEoW1br https://t.co/C6lNz8ov5D
+# Connexion à l'API de Twitter
+client = tweepy.Client(bearer_token=data["bearer_token"])
 
-Oui j’ai bien aimé comment le Barça a arrêté le Real et Vinicius en 3 rencontres cette saison 🤣 https://t.co/3EyZJUTCnz
+# Requête Twitter
+query = "Vinicius lang:fr -is:retweet -is:reply"
+tweets_fr = client.search_recent_tweets(query=query, tweet_fields=['text'], max_results=10)
 
-Supercoupe d’Espagne, le premier but de Vinicius est littéralement le même. Le défenseur était l’Abruti Suprême Araujo. Quel Barça ? https://t.co/daCsJFgbKI
+# Extraction des textes des tweets de la réponse
+tweets_text = [tweet.text for tweet in tweets_fr.data]
 
-Je pense que vous vous rendez pas compte à QUEL POINT Vinicius est devenu fort mdrrrrr sa palette offensive est encore plus impressionnante que l’année dernière c’est de la folie https://t.co/bImhlO2dAv
-
-Il peut gagner la LDC même 50x c’est une dinguerie de mettre Neymar et Vinicius dans la même phrase https://t.co/Au1OjGnCDJ
-
-Vinicius vs Bayern🤩
-
-Motm performance https://t.co/7GoajEpWXl
-
-Vinicius 🇧🇷 est le Brésilien le plus décisif dans l’histoire de la Ligue des Champions. 
-
-⚽️ | 31 buts et passes décisives
-👕 | 33 matchs
-⏳ | 3 dernières années
-
-NIVEAU BALLON D’OR. 🥶 https://t.co/8Aswp3LWOs
-
-La qualité de la passe de Toni Kroos à Vinicius #BAYRMA révèle l'importance des mathématiques dans la vie.
-
-Un jour il va falloir parler de la saison de vinicius jr 🇧🇷
-21 but et 11 passe décisives cette saison
-31 G/A https://t.co/HlSiLp5Vsw
-"""
-
-cleaned_tweets = clean_tweets(tweets)
-tokenized_tweets = tokenize_tweets(cleaned_tweets)
-removed_stopwords = remove_stopwords(tokenized_tweets)
+# Nettoyage des tweets
+cleaned_tweets = [clean_tweet(tweet) for tweet in tweets_text]
+tokenized_tweets = [tokenize_tweet(tweet) for tweet in cleaned_tweets]
+removed_stopwords = [remove_stopwords(tokens) for tokens in tokenized_tweets]
 
 print("Nettoyage : \n")
 print(cleaned_tweets)
@@ -51,3 +29,7 @@ print(tokenized_tweets)
 
 print("\nSuppression des mots inutiles :\n")
 print(removed_stopwords)
+
+
+# Appel de la fonction pour sauvegarder les résultats
+save_to_csv(tweets_text, removed_stopwords, 'tweets_removed_stopwords.csv')
--- fonctions.py
+++ fonctions.py
@@ -2,23 +2,32 @@ import re
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import csv
 
-nltk.download('punkt')
-nltk.download('stopwords')
 
-# Nettoyage des données
-def clean_tweets(tweets):
-    tweets = re.sub(r'http\S+', '', tweets)  # Supprimer les URL
-    tweets = re.sub(r'@\w+', '', tweets)     # Supprimer les mentions
-    tweets = re.sub(r'#\w+', '', tweets)     # Supprimer les hashtags
-    tweets = re.sub(r'[^\w\s]', '', tweets)  # Supprimer les caractères spéciaux
-    return tweets
+# Nettoyage des tweets
+def clean_tweet(tweet):
+    tweet = re.sub(r'http\S+', '', tweet)  # Supprimer les URL
+    tweet = re.sub(r'@\w+', '', tweet)     # Supprimer les mentions
+    tweet = re.sub(r'#\w+', '', tweet)     # Supprimer les hashtags
+    tweet = re.sub(r'[^\w\s]', '', tweet)  # Supprimer les caractères spéciaux
+    return tweet
 
 # Tokenisation
-def tokenize_tweets(tweets):
-    return word_tokenize(tweets)
+def tokenize_tweet(tweet):
+    return word_tokenize(tweet)
 
-# Filtration des stop-words
+# Filtration des stop-words : mots inutiles
 def remove_stopwords(tokens):
     stop_words = set(stopwords.words('french'))
     return [token for token in tokens if token.lower() not in stop_words]
+
+# Fonction pour sauvegarder les résultats au format CSV
+def save_to_csv(tweets,removed_stopwords, filename):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Tweet", "Removed Stopwords"])  # Écriture de l'en-tête
+        for i in range(len(tweets)):
+            writer.writerow([tweets[i], ' '.join(removed_stopwords[i])])  # Écriture du tweet et des stopwords supprimés
+
+
